a249a76edd1a62fb5695664ece6a440e892edcc5
Gabrieleabu23 <kaidgabriele23@gmail.com>
2024-03-12 10:22:47 +0100

    add: Front-end main scaffholding with vite-vue

---
+++ Front-end/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+// Import di Bootstrap 
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+createApp(App).mount('#app')
