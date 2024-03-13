2849a2bf5b2a92a0b3dfb312d803b58b3c8c7c22
Gabriele <gabriele.palma.bool@gmail.com>
2024-03-13 10:30:42 +0100

    fix

--- Front-end/src/components/prova.vue
+++ Front-end/src/components/prova.vue
@@ -6,23 +6,31 @@ export default {
 
 <template>
   <p>
+    <a
+      class="btn btn-primary"
+      data-bs-toggle="collapse"
+      href="#collapseExample"
+      role="button"
+      aria-expanded="false"
+      aria-controls="collapseExample"
+    >
+      Link with href
+    </a>
     <button
       class="btn btn-primary"
       type="button"
       data-bs-toggle="collapse"
-      data-bs-target="#collapseWidthExample"
+      data-bs-target="#collapseExample"
       aria-expanded="false"
-      aria-controls="collapseWidthExample"
+      aria-controls="collapseExample"
     >
-      Toggle width collapse
+      Button with data-bs-target
     </button>
   </p>
-  <div style="min-height: 120px">
-    <div class="collapse collapse-horizontal" id="collapseWidthExample">
-      <div class="card card-body" style="width: 300px">
-        This is some placeholder content for a horizontal collapse. It's hidden
-        by default and shown when triggered.
-      </div>
+  <div class="collapse" id="collapseExample">
+    <div class="card card-body">
+      Some placeholder content for the collapse component. This panel is hidden
+      by default but revealed when the user activates the relevant trigger.
     </div>
   </div>
 </template>
