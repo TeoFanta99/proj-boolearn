21e1eee3ee1366d2497eb3103b248689e66a2d82
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-03-14 16:15:06 +0100

    add fk

--- Back-end/database/migrations/9999_03_14_100116_add_foreign_keys.php
+++ Back-end/database/migrations/9999_03_14_100116_add_foreign_keys.php
@@ -13,7 +13,28 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema :: table('sponsorship_teacher', function(Blueprint $table){
+            $table -> foreignId('sponsorship_id') -> constrained();
+            $table -> foreignId('teacher_id') -> constrained();
+        });
+
+        Schema :: table('subject_teacher', function(Blueprint $table){
+            $table -> foreignId('subject_id') -> constrained();
+            $table -> foreignId('teacher_id') -> constrained();
+        });
+
+        Schema :: table('rating_teacher', function(Blueprint $table){
+            $table -> foreignId('rating_id') -> constrained();
+            $table -> foreignId('teacher_id') -> constrained();
+        });
+
+        Schema :: table('messages', function(Blueprint $table){
+            $table -> foreignId('teacher_id') -> constrained();
+        });
+
+        Schema :: table('reviews', function(Blueprint $table){
+            $table -> foreignId('teacher_id') -> constrained();
+        });
     }
 
     /**
@@ -23,6 +44,35 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema :: table('sponsorship_teacher', function(Blueprint $table){
+            $table -> dropForeign(['sponsorship_id']);
+            $table -> dropColumn(['sponsorship_id']);
+            $table -> dropForeign(['teacher_id']);
+            $table -> dropColumn(['teacher_id']);
+        });
+
+        Schema :: table('subject_teacher', function(Blueprint $table){
+            $table -> dropForeign(['subject_id']);
+            $table -> dropColumn(['subject_id']);
+            $table -> dropForeign(['teacher_id']);
+            $table -> dropColumn(['teacher_id']);
+        });
+
+        Schema :: table('rating_teacher', function(Blueprint $table){
+            $table -> dropForeign(['rating_id']);
+            $table -> dropColumn(['rating_id']);
+            $table -> dropForeign(['teacher_id']);
+            $table -> dropColumn(['teacher_id']);
+        });
+
+        Schema :: table('messages', function(Blueprint $table){
+            $table -> dropForeign(['teacher_id']);
+            $table -> dropColumn(['teacher_id']);
+        });
+
+        Schema :: table('reviews', function(Blueprint $table){
+            $table -> dropForeign(['teacher_id']);
+            $table -> dropColumn(['teacher_id']);
+        });
     }
 };
