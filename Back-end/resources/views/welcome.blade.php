3e0f37a6044bd77ba48d61562888c13d72359189
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-03-14 23:11:06 +0100

    add create btn

--- Back-end/resources/views/welcome.blade.php
+++ Back-end/resources/views/welcome.blade.php
@@ -1,15 +1,20 @@
 @extends('layouts.app')
 @section('content')
 
+<div class="button_container">
+    <button class="ms_button">
+        <a href="{{ route('user.create') }}">CREA</a>
+    </button>
+</div>
 
 <div class="container">
     <div class="row">
         <div class="col">
             <ul>
                 @foreach ($users as $user)
-                    <li>
-                        <a href="{{route('user.show' , $user->id)}}">{{$user->name}}</a>
-                    </li>
+                <li>
+                    <a href="{{route('user.show' , $user->id)}}">{{$user->name}}</a>
+                </li>
                 @endforeach
             </ul>
         </div>
