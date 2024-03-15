2e9c8e2f94234cd3048ce6184ee8345e74bd64ce
Martina Ambrosini <martinaambrosini17@gmail.com>
2024-03-15 15:54:35 +0100

    add logo e campi obbligatori

--- Back-end/resources/views/pages/create.blade.php
+++ Back-end/resources/views/pages/create.blade.php
@@ -17,22 +17,22 @@
     @method('PUT')
     <div class="row flex-column gap-4">
         <div class="col-3">
-            <label for="tax_id">P.IVA:</label>
+            <label for="tax_id">P.IVA <span style="color: red;  font-size: 1.5em;">*</span> :</label>
             <input type="text" id="tax_id" name="tax_id">
 
         </div>
         <div class="col-3">
             <div class="d-flex flex-column">
-                <label for="biography">Biografia</label>
+                <label for="biography">Biografia <span style="color: red;  font-size: 1.5em;">*</span></label>
                 <textarea name="biography" id="biography"></textarea>
             </div>
         </div>
         <div class="col-3">
-            <label for="city">Citt&agrave;:</label>
+            <label for="city">Citt&agrave; <span style="color: red;  font-size: 1.5em;">*</span> :</label>
             <input type="text" id="city" name="city">
         </div>
         <div class="col-3">
-            <label for="phone_number">Numero di telefono:</label>
+            <label for="phone_number">Numero di telefono <span style="color: red;  font-size: 1.5em;">*</span> :</label>
             <input type="number" id="phone_number" name="phone_number">
 
         </div>
@@ -56,6 +56,11 @@
 
 
         <div class="col-3"><input type="submit" value="CREA"></div>
+        
+        <br>
+
+        <span style="color: red;  font-size: 2em;">* i seguenti campi sono obbligatori</span>
+
 
 </form>
 {{-- @endauth --}}
