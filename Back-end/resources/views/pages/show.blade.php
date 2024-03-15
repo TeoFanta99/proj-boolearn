bbbb954f48aeb65fd2f48caa462955904f014724
Gabrieleabu23 <kaidgabriele23@gmail.com>
2024-03-15 09:40:28 +0100

    style:show

--- Back-end/resources/views/pages/show.blade.php
+++ Back-end/resources/views/pages/show.blade.php
@@ -11,25 +11,33 @@
     @if (session('error'))
         <div> {{ session('error') }}</div>
     @endif
-
-    <div>
-        {{ $teacher->user->name }}
-    </div>
-
-    <div>
-        {{ $teacher->user->lastname }}
-    </div>
-
-    <div>
-        {{ $teacher->user->date_of_birth }}
-    </div>
-
-    <div>
-        {{ $teacher->user->email }}
-    </div>
-    <div>
-        <img src="{{ asset('storage/' . $teacher->image_url) }}" alt="">
-    </div>
+        <div class="row">
+            <div class="col-4">
+                <div>
+                    {{ $teacher->user->name }}
+                </div>
+            
+                <div>
+                    {{ $teacher->user->lastname }}
+                </div>
+            
+                <div>
+                    {{ $teacher->user->date_of_birth }}
+                </div>
+            
+                <div>
+                    {{ $teacher->user->email }}
+                </div>
+               
+            </div>
+            <div class="col-3">
+                <div class="img_container">
+                    <img src="{{ asset('storage/' . $teacher->image_url) }}" alt="">
+                </div>
+            </div>
+        </div>
+   
+    
     {{-- @auth
         <form action="{{route('event.delete' , $event -> id)}}" method="POST">
         
