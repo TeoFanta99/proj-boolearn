98f12bfde55027b36fcebe9b7fe61e19c85a0366
Gabrieleabu23 <kaidgabriele23@gmail.com>
2024-03-15 18:18:46 +0100

    fix: console error app.js

--- Back-end/resources/views/layouts/app.blade.php
+++ Back-end/resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <script src="{{ asset('js/app.js') }}" defer></script>
+   
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -27,7 +27,7 @@
             <div class="container">
                 <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
                     <div class="logo_header">
-                       <img src="{{ Vite::asset('public/storage/images/BL1.svg') }}" alt="">
+                       <img src="{{ asset('public/storage/images/BL1.svg') }}" alt="">
                     </div>
                     {{-- config('app.name', 'Laravel') --}}
                 </a>
