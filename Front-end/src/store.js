7761f62bb008d5be2d63803d2e80537bdb09c2f8
Gabriele <gabriele.palma.bool@gmail.com>
2024-03-18 14:23:57 +0100

    fix

--- Front-end/src/store.js
+++ Front-end/src/store.js
@@ -3,5 +3,10 @@ import { reactive } from "vue";
 export const store = reactive({
   List: [],
   apiURL: "http://127.0.0.1:8000/api/v1/teachers",
-  view:0,
+  view: 0,
+  NameSurname: "",
+  Email: "",
+  Title: "",
+  Message: "",
+  image_url: "",
 });
