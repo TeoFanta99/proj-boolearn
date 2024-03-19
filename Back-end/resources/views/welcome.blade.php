2eae925d5f5e427a41addee6df047931d177bed8
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-03-19 09:31:07 +0100

    tradotto scritta in italiano

--- Back-end/resources/views/welcome.blade.php
+++ Back-end/resources/views/welcome.blade.php
@@ -12,14 +12,15 @@
         @foreach ($teachers as $teacher)
         {{-- VERIFICO SE l'UTENTE E' LOGGATO --}}
         @if (Auth::check())
-        {{-- VERIFICO SE  l'ID DELL'UTENTE LOGGATO CORRISPONDE ALL'USER ID DEI TEACHER - HA UN PROFILO --}}
+        {{-- VERIFICO SE l'ID DELL'UTENTE LOGGATO CORRISPONDE ALL'USER ID DEI TEACHER - HA UN PROFILO --}}
         @if (Auth::user()->id == $teacher->user_id)
-        {{-- STAMPO I SUOI PROFILI  --}}
+        {{-- STAMPO I SUOI PROFILI --}}
         <div class="col-3 p-2">
             <a href="{{ route('user.show', $teacher->user->id) }}" class="text-decoration-none">
                 <div class="card pt-3 border-0 shadow">
                     <div class="d-flex justify-content-center align-items-center" style="height: 200px;">
-                        <img class="object-fit-cover border rounded" style="height: 100%;" src="{{ asset('storage/' . $teacher->image_url) }}" alt="">
+                        <img class="object-fit-cover border rounded" style="height: 100%;"
+                            src="{{ asset('storage/' . $teacher->image_url) }}" alt="">
                     </div>
                     <div class="card-body">
                         <h4>{{ $teacher->user->name }} {{ $teacher->user->lastname }}</h4>
@@ -28,20 +29,23 @@
             </a>
         </div>
         <a href="{{ route('user.edit', $teacher->user->id) }}">Gestisci Profilo</a>
-        <form class="ms-3 d-block" action="{{ route('user.del', $teacher->user->id) }}" method="POST" onsubmit="return confirm('Confermare?');">
+        <form class="ms-3 d-block" action="{{ route('user.del', $teacher->user->id) }}" method="POST"
+            onsubmit="return confirm('Confermare?');">
             @csrf
             @method('DELETE')
-            <input type="submit" value="DELETE">
+            <input type="submit" value="CANCELLA">
         </form>
         @endif
-        {{-- SE NESSUNO E' LOGGATO  --}}
+        {{-- SE NESSUNO E' LOGGATO --}}
         @else
         {{-- STAMPA TUTTI --}}
         <div class="col-3 p-2">
             <a href="{{ route('user.show', $teacher->user->id) }}" class="text-decoration-none">
                 <div class="card pt-3 border-0 shadow">
-                    <div class="d-flex justify-content-center align-items-center img_circle mx-auto" style="height: 250px;">
-                        <img class="w-100 h-100 rounded-circle" src="{{ asset('storage/' . $teacher->image_url) }}" alt="">
+                    <div class="d-flex justify-content-center align-items-center img_circle mx-auto"
+                        style="height: 250px;">
+                        <img class="w-100 h-100 rounded-circle" src="{{ asset('storage/' . $teacher->image_url) }}"
+                            alt="">
                     </div>
                     <div class="card-body">
                         <h4>{{ $teacher->user->name }} {{ $teacher->user->lastname }}</h4>
@@ -49,11 +53,10 @@
                 </div>
             </a>
         </div>
-        {{-- <li class="col-3"><a
-                            href="{{ route('user.show', $teacher->user->id) }}">{{ $teacher->user->name }}
-                            {{ $teacher->user->lastname }}</a>
+        {{-- <li class="col-3"><a href="{{ route('user.show', $teacher->user->id) }}">{{ $teacher->user->name }}
+                {{ $teacher->user->lastname }}</a>
 
-                </li> --}}
+        </li> --}}
         @endif
         @endforeach
     </div>
