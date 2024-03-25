9092d888188f7bbc320152fe7dd8d0076dc433e5
Gabrieleabu23 <kaidgabriele23@gmail.com>
2024-03-25 22:51:13 +0100

    add: cv preview

--- Back-end/resources/views/pages/edit.blade.php
+++ Back-end/resources/views/pages/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <h1>Modifica Profilo</h1>
-    
+
     {{-- @auth --}}
     @if ($errors->any())
         <div class="alert alert-danger">
@@ -25,11 +25,12 @@
         @method('PUT')
         <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
 
-         <div class="container">
+        <div class="container">
             <div class="row gutters">
                 <div class="col-xl-4 col-lg-3 col-md-12 col-sm-12 col-12 mb-3">
                     <div class="card p-0 h-20">
-                        <div class="card-header bg-transparent border-primary text-primary" style="font-weight: bold">FOTO PROFILO
+                        <div class="card-header bg-transparent border-primary text-primary" style="font-weight: bold">FOTO
+                            PROFILO
                         </div>
                         <div class="card-body border-dark p-3 m-0 d-flex flex-row justify-content-center">
                             <div class="account-settings">
@@ -43,20 +44,30 @@
                             </div>
                         </div>
                     </div>
+                    <div class="col-12">
+                        <div style="position: relative;">
+                            <a href="{{ route('teacher.index', Auth::user()->id) }}" target="_blank" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 1;"></a>
+                            <div style="position: absolute; top: 0; left: 0; width: 100%; height: 20px; background-color: black; z-index: 2;"></div>
+                            <embed src="{{ asset('storage/' . $teacher->cv_url) }}" type="application/pdf" width="100%" height="400px" style="position: relative; z-index: 0; display: none;">
+                          </div>
+
                     </div>
-                    <div class="col-xl-8 col-lg-9 col-md-12 col-sm-12 col-12">
+                </div>
+                <div class="col-xl-8 col-lg-9 col-md-12 col-sm-12 col-12">
                     <div class="card h-100">
-                        <div class="card-header bg-transparent border-primary text-primary" style="font-weight: bold">DETTAGLI PROFILO
+                        <div class="card-header bg-transparent border-primary text-primary" style="font-weight: bold">
+                            DETTAGLI PROFILO
                         </div>
                         <div class="card-body">
                             <div class="row gutters">
                                 <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                                    <div class="form-group"> 
+                                    <div class="form-group">
                                         <div class="form-group">
-                                            <label for="phone_number">TELEFONO 
-                                                <span style="color: red; font-size: 1.5em;">*</span> 
+                                            <label for="phone_number">TELEFONO
+                                                <span style="color: red; font-size: 1.5em;">*</span>
                                             </label>
-                                            <input type="number" class="form-control" id="phone_number" name="phone_number" value="{{ $teacher->phone_number }}"">
+                                            <input type="number" class="form-control" id="phone_number" name="phone_number"
+                                                value="{{ $teacher->phone_number }}"">
                                             <span id="PhoneNo"class="text-danger d-none"></span>
                                         </div>
                                     </div>
@@ -64,7 +75,8 @@
                                 <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                                     <div class="form-group">
                                         <div class="form-group">
-                                            <label for="city">CITTÀ <span style="color: red;  font-size: 1.5em;">*</span></label>
+                                            <label for="city">CITTÀ <span
+                                                    style="color: red;  font-size: 1.5em;">*</span></label>
                                             <select id="city" class="form-control" name="city">
                                                 <option value="{{ $teacher->city }}" selected>{{ $teacher->city }}</option>
                                             </select>
@@ -75,52 +87,61 @@
                             <div class="row gutters">
                                 <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                                     <div class="form-group">
-                                        <label for="tax_id">P.IVA <span style="color: red;  font-size: 1.5em;">*</span></label>
-                                        <input type="text" class="form-control" id="tax_id" name="tax_id" value="{{ $teacher->tax_id }}">
-                                        <span id="taxNo"class="text-danger d-none"></span> 
+                                        <label for="tax_id">P.IVA <span
+                                                style="color: red;  font-size: 1.5em;">*</span></label>
+                                        <input type="text" class="form-control" id="tax_id" name="tax_id"
+                                            value="{{ $teacher->tax_id }}">
+                                        <span id="taxNo"class="text-danger d-none"></span>
                                     </div>
                                 </div>
                                 <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                                     <div class="form-group">
-                                        <label for="cv_url">CARICA CV <span style="color: red;  font-size: 1.5em;"> *</span></label>
-                                        <input type="file" class="form-control" id="cv_url" name="cv_url"  accept=".pdf">          
+                                        <label for="cv_url">CARICA CV <span style="color: red;  font-size: 1.5em;">
+                                                *</span></label>
+                                        <input type="file" class="form-control" id="cv_url" name="cv_url"
+                                            accept=".pdf">
                                     </div>
-                                    <a href="{{ route('teacher.index', Auth::user()->id) }}" target="_blank">CV</a>
+
                                 </div>
                                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                                     <div class="form-group">
                                         <div class="d-flex flex-column">
-                                            <label for="biography">BIOGRAFIA <span style="color: red;  font-size: 1.5em;">*</span></label>
+                                            <label for="biography">BIOGRAFIA <span
+                                                    style="color: red;  font-size: 1.5em;">*</span></label>
                                             <textarea name="biography" class="form-control" id="biography">{{ $teacher->biography }}</textarea>
                                             <span id="bioNo"class="text-danger d-none"></span>
-                                        </div>      
+                                        </div>
                                     </div>
                                 </div>
                                 <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
                                     <div class="form-group mb-3">
-                                        <label class="mb-2">SELEZIONA MATERIE  <span style="color: red; font-size: 1.5em;"> *</span></label>
+                                        <label class="mb-2">SELEZIONA MATERIE <span
+                                                style="color: red; font-size: 1.5em;"> *</span></label>
                                         <br>
 
                                         @foreach ($subjects as $subject)
-                                            <input type="checkbox" class="form-check-input"  id="subject{{ $subject->id }}" name="subjects[]"
-                                                value="{{ $subject->id }}" {{ $teacher->subjects->contains($subject->id) ? 'checked' : '' }}>
+                                            <input type="checkbox" class="form-check-input"
+                                                id="subject{{ $subject->id }}" name="subjects[]"
+                                                value="{{ $subject->id }}"
+                                                {{ $teacher->subjects->contains($subject->id) ? 'checked' : '' }}>
                                             <label for="subject{{ $subject->id }}">{{ $subject->name }}</label><br>
-                                        @endforeach          
+                                        @endforeach
                                     </div>
                                 </div>
                             </div>
                             <div class="row gutters">
                                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                                     <div class="form-group">
-                                       <label for="motto">MOTTO</label>
-                                       <textarea class="form-control" id="motto" name="motto">{{ $teacher->motto }}</textarea>
+                                        <label for="motto">MOTTO</label>
+                                        <textarea class="form-control" id="motto" name="motto">{{ $teacher->motto }}</textarea>
                                     </div>
                                 </div>
                             </div>
                             <div class="row gutters">
                                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                                     <div class="d-flex justify-content-between mt-4 text-right">
-                                         <div style="color: red; font-size: 1em; text-align: left; font-weight:bold; ">* I seguenti campi sono obbligatori</div>
+                                        <div style="color: red; font-size: 1em; text-align: left; font-weight:bold; ">* I
+                                            seguenti campi sono obbligatori</div>
                                         <input type="submit" class="btn btn-primary" value="AGGIORNA PROFILO">
                                     </div>
                                 </div>
@@ -167,26 +188,26 @@
 
             let validation = 0;
             var teachers = <?php echo json_encode($teachers); ?>;
-            var teacherSingle= <?php echo json_encode($teacher); ?>;
+            var teacherSingle = <?php echo json_encode($teacher); ?>;
             let taxID = document.getElementById('tax_id').value;
             let bio = document.getElementById('biography').value;
             let phone = document.getElementById('phone_number');
 
-            validation += checkTaxID(taxID, teachers,teacherSingle); // Non è necessario passare teacher qui
+            validation += checkTaxID(taxID, teachers, teacherSingle); // Non è necessario passare teacher qui
             validation += checkBio(bio);
-            validation += checkPhone(phone_number,teacherSingle);
+            validation += checkPhone(phone_number, teacherSingle);
 
             if (validation == 3) {
                 this.submit();
             }
 
 
-            function checkPhone(phone,teacherSingle) {
+            function checkPhone(phone, teacherSingle) {
                 let isUnique = true;
 
-                
+
                 for (let i = 0; i < teachers.length; i++) {
-                    if(phone.value == teachers[i].phone_number && teachers[i].id ==teacherSingle.id ){
+                    if (phone.value == teachers[i].phone_number && teachers[i].id == teacherSingle.id) {
                         break;
                     }
                     if (phone.value === teachers[i].phone_number) {
@@ -226,13 +247,13 @@
                 return yesOrNo;
             }
 
-            function checkTaxID(taxID, teachers,teacher) {
+            function checkTaxID(taxID, teachers, teacher) {
                 let isUnique = true;
 
                 // Cicla attraverso gli insegnanti
                 for (let i = 0; i < teachers.length; i++) {
                     // Controlla se la partita IVA è un duplicato
-                    if(taxID == teacher.tax_id){
+                    if (taxID == teacher.tax_id) {
                         break;
                     }
                     if (taxID === teachers[i].tax_id) {
