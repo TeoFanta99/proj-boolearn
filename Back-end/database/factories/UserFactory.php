2d9c9e4bcf4deb731a6a0a5dcf7e06ab4b5b3269
Gabrieleabu23 <kaidgabriele23@gmail.com>
2024-03-28 05:00:24 +0100

    fix: better mails and age in factory

--- Back-end/database/factories/UserFactory.php
+++ Back-end/database/factories/UserFactory.php
@@ -22,14 +22,15 @@ class UserFactory extends Factory
          $name = $this->faker->firstName($gender);
          $faker = FakerFactory::create('it_IT');
          $city = $faker->city;
+         $lastname=$this->faker->lastName();
         return [
             'name' => $name,
-            'email' => $this->faker->safeEmail(),
+            'email' => $name . '.' . $lastname  .fake()->numberBetween(1,9).'@example.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'lastname' => $this->faker->lastName(),
-            'date_of_birth' => $this->faker->dateTimeBetween('-60 years', '-18 years'),
+            'lastname' => $lastname,
+            'date_of_birth' => $this->faker->dateTimeBetween('-40 years', '-21 years'),
             'gender' => $gender,
             'city' =>$city,
         ];
