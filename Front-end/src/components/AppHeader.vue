8e0b605de2fc53517acc38030afd1747569a3e17
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-03-29 14:34:22 +0100

    aggiunte modifiche

--- Front-end/src/components/AppHeader.vue
+++ Front-end/src/components/AppHeader.vue
@@ -1,6 +1,26 @@
 <script>
 export default {
   name: "AppHeader",
+  data() {
+    return {
+      showOffcanvas: false
+    }
+  },
+
+  methods: {
+    handleLoginClick() {
+      if (window.innerWidth <= 992) {
+        // Apri l'offcanvas solo su schermi piccoli
+        this.showOffcanvas = true;
+      } else {
+        // Apri la pagina di login su schermi grandi
+        window.open("http://localhost:8000/login", "_blank");
+      }
+    },
+    closeOffcanvas() {
+      this.showOffcanvas = false; // Chiudi l'offcanvas
+    }
+  },
 };
 </script>
 
@@ -13,12 +33,20 @@ export default {
         <a href="http://localhost:8000/register" class="d-lg-block d-none mt-1 ms-4" target="_blank"><i
             class="fa-solid fa-user-plus" style="color: rgb(83, 83, 255); font-size: 30px"></i></a>
       </div>
+
+
+
+
       <div>
         <button class="btn d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
           aria-controls="offcanvasRight">
           <i class="fa-solid fa-circle-user" style="color: #b3b3b3; font-size: 40px"></i>
         </button>
       </div>
+
+
+
+      
 
       <div class="offcanvas offcanvas-start offcanvas-lg d-lg-none" tabindex="-1" id="offcanvasRight"
         aria-labelledby="offcanvasRightLabel">
@@ -29,70 +57,48 @@ export default {
         <div class="offcanvas-body">
           <a href="http://localhost:8000/login" class="btn btn-primary d-block w-50">Login</a>
           <a href="http://localhost:8000/register" class="btn btn-primary d-block w-50 mt-2">Registrazione</a>
-          <a href="http://localhost:8000/login" class="btn btn-primary d-block w-50">Login</a>
-          <a href="http://localhost:8000/register" class="btn btn-primary d-block w-50 mt-2">Registrazione</a>
         </div>
       </div>
-      <div class="home">
-        <router-link :to="{ name: 'home' }" class="d-block d-lg-none">
-          <img src="../../public/BLearn.svg" alt="LOGO" class="logo-mobile" />
-        </router-link>
-
-        <router-link :to="{ name: 'home' }" class="d-none d-lg-block">
-          <div class="d-lg-flex justify-content-center logo-desktop">
-            <img src="../../public/Crop_BooLEARN.svg" alt="LOGO DESKTOP" class="w-100 h-100 object-fit-contain" />
-          </div>
-        </router-link>
-      </div>
+      
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
         aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse d-lg-none" id="navbarText">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-center justify-content-lg-end">
-          <li class="nav-item">
-            <a class="nav-link text-center" href="#">
-              <router-link :to="{ name: 'about' }"><i class="fa-solid fa-people-group fa-lg me-2"></i>IL NOSTRO
-                TEAM</router-link>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-center" href="#">
-              <router-link :to="{ name: 'faq' }"><i class="fa-solid fa-question fa-lg me-2"></i>
-                F.A.Q</router-link>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-center me-3" href="#">
-              <router-link :to="{ name: 'contact' }">
-                <i class="fa-solid fa-comments fa-lg me-2"></i> CONTATTACI
-              </router-link>
-            </a>
-          </li>
-        </ul>
+
       </div>
     </nav> -->
 
 
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid d-flex justify-content-between">
+      <div class="container-fluid d-flex justify-content-lg-between justify-content-around">
+
+        <!-- BOTTONI LOGIN E REGISTER -->
         <div class="d-flex gap-2">
-          <a href="http://localhost:8000/login" class="d-lg-inline-block ms-4" target="_blank"><i
+          <a @click="handleLoginClick" href="http://localhost:8000/login" class="d-lg-inline-block ms-4" target="_blank"
+            data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample"><i
               class="fa-solid fa-circle-user" style="color: #b3b3b3; font-size: 40px"></i></a>
-          <a href="http://localhost:8000/register" class="d-lg-block d-none mt-1 ms-4" target="_blank"><i
+          <a href="http://localhost:8000/register" class="d-lg-inline-block d-none mt-1 ms-4" target="_blank"><i
               class="fa-solid fa-user-plus" style="color: rgb(83, 83, 255); font-size: 30px"></i></a>
+        </div>
+
+        <!-- OFFCANVAS -->
+        <div class="offcanvas offcanvas-start d-none" tabindex="-1" id="offcanvasExample"
+          aria-labelledby="offcanvasExampleLabel" :class="{ 'show': showOffcanvas }">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+            <button type="button" class="btn-close text-reset" @click="closeOffcanvas" data-bs-dismiss="offcanvas"
+              aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            Contenuto dell'offcanvas...
+          </div>
         </div>
 
         <!-- LOGO HOME -->
         <div class="home">
-          <router-link :to="{ name: 'home' }" class="d-block d-lg-none">
+          <router-link :to="{ name: 'home' }">
             <img src="../../public/BLearn.svg" alt="LOGO" class="logo-mobile" />
-          </router-link>
-
-          <router-link :to="{ name: 'home' }" class="d-none d-lg-block">
-            <div class="d-lg-flex justify-content-center logo-desktop">
-              <img src="../../public/Crop_BooLEARN.svg" alt="LOGO DESKTOP" class="w-100 h-100 object-fit-contain" />
-            </div>
           </router-link>
         </div>
 
