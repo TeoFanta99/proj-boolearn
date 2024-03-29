eab3a0616c44b4038085a4911818d4d40204b598
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-03-29 13:09:42 +0100

    elementi sono allineati, manca fix al logo e aggiungere offcanvas

--- Front-end/src/components/AppHeader.vue
+++ Front-end/src/components/AppHeader.vue
@@ -6,126 +6,60 @@ export default {
 
 <template>
   <header>
-    <nav
-      class="navbar navbar-expand-lg bg-body-tertiary d-flex justify-content-between h-100"
-    >
+    <!-- <nav class="navbar navbar-expand-lg bg-body-tertiary d-flex justify-content-between h-100">
       <div class="d-flex gap-2">
-        <a
-          href="http://localhost:8000/login"
-          class="d-lg-block d-none ms-4"
-          target="_blank"
-          ><i
-            class="fa-solid fa-circle-user"
-            style="color: #b3b3b3; font-size: 40px"
-          ></i
-        ></a>
-        <a
-          href="http://localhost:8000/register"
-          class="d-lg-block d-none mt-1 ms-4"
-          target="_blank"
-          ><i
-            class="fa-solid fa-user-plus"
-            style="color: rgb(83, 83, 255); font-size: 30px"
-          ></i
-        ></a>
+        <a href="http://localhost:8000/login" class="d-lg-block d-none ms-4" target="_blank"><i
+            class="fa-solid fa-circle-user" style="color: #b3b3b3; font-size: 40px"></i></a>
+        <a href="http://localhost:8000/register" class="d-lg-block d-none mt-1 ms-4" target="_blank"><i
+            class="fa-solid fa-user-plus" style="color: rgb(83, 83, 255); font-size: 30px"></i></a>
       </div>
       <div>
-        <button
-          class="btn d-lg-none"
-          type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasRight"
-          aria-controls="offcanvasRight"
-        >
-          <i
-            class="fa-solid fa-circle-user"
-            style="color: #b3b3b3; font-size: 40px"
-          ></i>
+        <button class="btn d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
+          aria-controls="offcanvasRight">
+          <i class="fa-solid fa-circle-user" style="color: #b3b3b3; font-size: 40px"></i>
         </button>
       </div>
-      <!-- Off-canvas -->
-      <div
-        class="offcanvas offcanvas-start offcanvas-lg d-lg-none"
-        tabindex="-1"
-        id="offcanvasRight"
-        aria-labelledby="offcanvasRightLabel"
-      >
+
+      <div class="offcanvas offcanvas-start offcanvas-lg d-lg-none" tabindex="-1" id="offcanvasRight"
+        aria-labelledby="offcanvasRightLabel">
         <div class="offcanvas-header border-bottom border-1">
           Login/Registrazione
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="offcanvas"
-            aria-label="Close"
-          ></button>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
-          <a
-            href="http://localhost:8000/login"
-            class="btn btn-primary d-block w-50"
-            >Login</a
-          >
-          <a
-            href="http://localhost:8000/register"
-            class="btn btn-primary d-block w-50 mt-2"
-            >Registrazione</a
-          >
-          <a
-            href="http://localhost:8000/login"
-            class="btn btn-primary d-block w-50"
-            >Login</a
-          >
-          <a
-            href="http://localhost:8000/register"
-            class="btn btn-primary d-block w-50 mt-2"
-            >Registrazione</a
-          >
+          <a href="http://localhost:8000/login" class="btn btn-primary d-block w-50">Login</a>
+          <a href="http://localhost:8000/register" class="btn btn-primary d-block w-50 mt-2">Registrazione</a>
+          <a href="http://localhost:8000/login" class="btn btn-primary d-block w-50">Login</a>
+          <a href="http://localhost:8000/register" class="btn btn-primary d-block w-50 mt-2">Registrazione</a>
         </div>
       </div>
       <div class="home">
         <router-link :to="{ name: 'home' }" class="d-block d-lg-none">
           <img src="../../public/BLearn.svg" alt="LOGO" class="logo-mobile" />
         </router-link>
-        <!-- <span>BOOLEARN</span> -->
+
         <router-link :to="{ name: 'home' }" class="d-none d-lg-block">
           <div class="d-lg-flex justify-content-center logo-desktop">
-            <img
-              src="../../public/Crop_BooLEARN.svg"
-              alt="LOGO DESKTOP"
-              class="w-100 h-100 object-fit-contain"
-            />
+            <img src="../../public/Crop_BooLEARN.svg" alt="LOGO DESKTOP" class="w-100 h-100 object-fit-contain" />
           </div>
         </router-link>
       </div>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarText"
-        aria-controls="navbarText"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+        aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse d-lg-none" id="navbarText">
-        <ul
-          class="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-center justify-content-lg-end"
-        >
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-center justify-content-lg-end">
           <li class="nav-item">
             <a class="nav-link text-center" href="#">
-              <router-link :to="{ name: 'about' }"
-                ><i class="fa-solid fa-people-group fa-lg me-2"></i>IL NOSTRO
-                TEAM</router-link
-              >
+              <router-link :to="{ name: 'about' }"><i class="fa-solid fa-people-group fa-lg me-2"></i>IL NOSTRO
+                TEAM</router-link>
             </a>
           </li>
           <li class="nav-item">
             <a class="nav-link text-center" href="#">
-              <router-link :to="{ name: 'faq' }"
-                ><i class="fa-solid fa-question fa-lg me-2"></i>
-                F.A.Q</router-link
-              >
+              <router-link :to="{ name: 'faq' }"><i class="fa-solid fa-question fa-lg me-2"></i>
+                F.A.Q</router-link>
             </a>
           </li>
           <li class="nav-item">
@@ -136,6 +70,61 @@ export default {
             </a>
           </li>
         </ul>
+      </div>
+    </nav> -->
+
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid d-flex justify-content-between">
+        <div class="d-flex gap-2">
+          <a href="http://localhost:8000/login" class="d-lg-inline-block ms-4" target="_blank"><i
+              class="fa-solid fa-circle-user" style="color: #b3b3b3; font-size: 40px"></i></a>
+          <a href="http://localhost:8000/register" class="d-lg-block d-none mt-1 ms-4" target="_blank"><i
+              class="fa-solid fa-user-plus" style="color: rgb(83, 83, 255); font-size: 30px"></i></a>
+        </div>
+
+        <!-- LOGO HOME -->
+        <div class="home">
+          <router-link :to="{ name: 'home' }" class="d-block d-lg-none">
+            <img src="../../public/BLearn.svg" alt="LOGO" class="logo-mobile" />
+          </router-link>
+
+          <router-link :to="{ name: 'home' }" class="d-none d-lg-block">
+            <div class="d-lg-flex justify-content-center logo-desktop">
+              <img src="../../public/Crop_BooLEARN.svg" alt="LOGO DESKTOP" class="w-100 h-100 object-fit-contain" />
+            </div>
+          </router-link>
+        </div>
+
+        <!-- BOTTONE ESPANDIBILE (HAMBURGER MENU) -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link text-center" href="#">
+                <router-link :to="{ name: 'about' }"><i class="fa-solid fa-people-group fa-lg me-2"></i>IL NOSTRO
+                  TEAM</router-link>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-center" href="#">
+                <router-link :to="{ name: 'faq' }"><i class="fa-solid fa-question fa-lg me-2"></i>
+                  F.A.Q</router-link>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-center me-3" href="#">
+                <router-link :to="{ name: 'contact' }">
+                  <i class="fa-solid fa-comments fa-lg me-2"></i> CONTATTACI
+                </router-link>
+              </a>
+            </li>
+          </ul>
+
+        </div>
       </div>
     </nav>
   </header>
