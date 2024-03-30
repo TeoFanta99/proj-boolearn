79cf9ea15bacde1b241bf1d9e6beacf7d01b142f
Gabriele <gabriele.palma.bool@gmail.com>
2024-03-30 18:00:47 +0100

    add

--- Back-end/database/seeders/ReviewTableSeeder.php
+++ Back-end/database/seeders/ReviewTableSeeder.php
@@ -16,10 +16,50 @@ class ReviewTableSeeder extends Seeder
      */
     public function run()
     {
-        Review :: factory() -> count(100) -> make() -> each(function($review) {
-            $teacher = Teacher :: inRandomOrder() -> first();
-            $review -> teacher() -> associate($teacher);
-            $review -> save();
-        });
+       
+        // Carica il contenuto del file JSON con le materie
+        $jsonReviews = file_get_contents(public_path('reviews.json'));
+        $reviewsJSON = json_decode($jsonReviews, true);
+
+        
+
+
+        for($i = 0; $i < 10; $i++){
+
+            foreach ($reviewsJSON as $reviews) {
+
+                // Ciclo su ciascuna materia della categoria
+                foreach ($reviews as $review) {
+                    
+                    // Creare un array per memorizzare gli insegnanti associati a titoli di recensioni
+                    $assignedTeachers = [];
+                    
+                    // Cerca un insegnante non associato alla recensione attuale
+                    $teacher = Teacher::inRandomOrder()->whereNotIn('id', $assignedTeachers)->first();
+
+                    // Crea una nuova istanza di Subject e la salva nel database
+                    $newReview = new Review();
+                    $newReview->title = $review['nome'];
+                    $newReview->description = $review['descrizione'];
+                    $newReview->email = $review['mail'];
+                    $newReview->date_of_review = $review['data'];
+                
+
+                    $newReview -> teacher() -> associate($teacher);
+                    $newReview->save();
+
+                    // Aggiungi l'id dell'insegnante associato all'array
+                    $assignedTeachers[] = $teacher->id;
+                }
+            }
+
+        }
+       
+
+        // Review :: factory() -> count(100) -> make() -> each(function($review) {
+        //     $teacher = Teacher :: inRandomOrder() -> first();
+        //     $review -> teacher() -> associate($teacher);
+        //     $review -> save();
+        // });
     }
 }
