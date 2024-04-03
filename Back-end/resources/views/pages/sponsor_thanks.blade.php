143b84b6871790f0e168487f800b8b654c547dfe
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-04-03 09:25:44 +0200

    add btn redirect

--- Back-end/resources/views/pages/sponsor_thanks.blade.php
+++ Back-end/resources/views/pages/sponsor_thanks.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 @section('content')
 
-GRAZIEE!
+<h2>ACQUISTO EFFETTUATO CORRETTAMENTE, GRAZIE!</h2>
+
+<button class="btn btn-primary me-3 mt-2 w-100">
+    <a href="{{ route('welcome') }}" style="text-decoration: none; color: white">
+        TORNA ALLA HOME
+    </a>
+</button>
 @endsection
