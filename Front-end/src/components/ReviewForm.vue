22b229790642eb4cab2a77e97204ecf1792ee237
Gabriele <gabriele.palma.bool@gmail.com>
2024-04-03 10:06:06 +0200

    add

--- Front-end/src/components/ReviewForm.vue
+++ Front-end/src/components/ReviewForm.vue
@@ -19,8 +19,10 @@ export default {
         title: store.ReviewTitle,
         user_email: store.UserEmail,
         description: store.EmailMessage,
-        teacher_id: store.List.user.id,
+        teacher_id: store.List.id,
       };
+
+      console.log(DatatoSend);
       axios
         .post("http://127.0.0.1:8000/api/v1/review", DatatoSend)
 
@@ -47,24 +49,54 @@ export default {
   <form @submit.prevent="test">
     <div class="mb-3">
       <label class="form-label">TITOLO DELLA RECENSIONE</label>
-      <input v-model="store.ReviewTitle" type="text" name="title" id="title" class="form-control"
-        placeholder="Dai un titolo alla recensione" />
+      <input
+        v-model="store.ReviewTitle"
+        type="text"
+        name="title"
+        id="title"
+        class="form-control"
+        placeholder="Dai un titolo alla recensione"
+      />
     </div>
     <div class="mb-3">
       <label class="form-label">INDIRIZZO EMAIL</label>
-      <input v-model="store.UserEmail" type="email" name="user_email" id="email" class="form-control"
-        placeholder="Inserisci la tua email" />
+      <input
+        v-model="store.UserEmail"
+        type="email"
+        name="user_email"
+        id="email"
+        class="form-control"
+        placeholder="Inserisci la tua email"
+      />
     </div>
     <label class="form-label">TESTO DELLA RECENSIONE</label>
-    <textarea v-model="store.EmailMessage" class="form-control" style="height: 200px" name="description"
-      id="description" placeholder="Digita qui la tua recensione"></textarea>
+    <textarea
+      v-model="store.EmailMessage"
+      class="form-control"
+      style="height: 200px"
+      name="description"
+      id="description"
+      placeholder="Digita qui la tua recensione"
+    ></textarea>
     <div class="d-flex justify-content-center">
-      <input type="submit" class="mt-3 mb-3 btn btn-primary w-75" value="AGGIUNGI RECENSIONE" />
+      <input
+        type="submit"
+        class="mt-3 mb-3 btn btn-primary w-75"
+        value="AGGIUNGI RECENSIONE"
+      />
     </div>
   </form>
 
-  <div class="d-flex justify-content-center mt-4" v-if="success"
-    style="color: green; font-weight: bolder; font-size: 25px; text-align: center;">
+  <div
+    class="d-flex justify-content-center mt-4"
+    v-if="success"
+    style="
+      color: green;
+      font-weight: bolder;
+      font-size: 25px;
+      text-align: center;
+    "
+  >
     RECENSIONE INVIATA CON SUCCESSO!
   </div>
 </template>
